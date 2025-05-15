0a7de99d8a78637dbf6e527e5025cefd81f5ab42
PyDevC <anuskcse@gmail.com>
2025-05-15 22:56:43 +0530

    Event kill and wait

--- lua/src/events.lua
+++ lua/src/events.lua
@@ -1,11 +1,15 @@
+local exit = os.exit
+
 local M = {}
 
 M.Event = {
     EventType = 0,
 }
 
+M.WaitQueue = {} -- in executor check whether the event box is in wait or not
+
 M.EventBox = {
-    events = nil,         -- can be one or more events
+    events = nil,     -- can be one or more events
     interupt = false, -- if an event if active in the event box that could interupt all processes
     ignore = nil
 }
@@ -17,10 +21,31 @@ function NewEventBox(EventBox, Event)
     EventBox.events = { Event.Event }
 end
 
-function Wait(Event)
+---@param EventBox table
+---@return integer
+--Puts the EventBox into waiting queue
+--All the events inside EventBox will stop and wait till released
+--If the EventBox has interupt in it then it command to kills all the event
+function Wait(EventBox)
+    if EventBox.interupt then
+        Kill(EventBox)
+    end
+    table.insert(M.WaitQueue, EventBox)
+    return 0
 end
 
-function Kill(Event)
+---@param EventBox table
+--Releases the first event that was inserted
+function Release(EventBox)
+    table.remove(M.WaitQueue, 0)
+end
+
+function Kill(EventBox)
+    if EventBox.interupt then
+        EventBox = nil
+        M.WaitQueue = nil
+        exit(1)
+    end
 end
 
 return M
